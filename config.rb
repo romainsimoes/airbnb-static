7d612ad9d99fb7af8c7f08ed11fcd36fca14a790
romainsimoes <romain.simoes.pro@gmail.com>
2016-08-01 16:22:23 +0200

    Flat dynamic view

--- config.rb
+++ config.rb
@@ -10,8 +10,13 @@
 # Auto-prefixing of CSS code with vendor prefix
 activate :autoprefixer
 
+["anne", "seb", "romain"].each do |name|
+  proxy "/flats/#{name}.html", "/flats/show.html", :locals => { :owner => name }, :ignore => true
+end
+
 ###
 # Page options, layouts, aliases and proxies
+#
 ###
 
 # Per-page layout changes:
